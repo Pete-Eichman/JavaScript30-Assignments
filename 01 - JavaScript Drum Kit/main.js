aeb4dde48271425065d7a86874a71b88d55f5807
Pete Eichman <peichman14@gmail.com>
2017-03-06 15:45:57 -0500

    Keys now light up but do not shut off.

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,14 +1,15 @@
 window.addEventListener('keydown', function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if(!audio) return;
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-
+  key.classList.add('playing');
 });
 
 function removeTransition(e) {
-  console.log(e);
-};
+  if (e.propertyName !== 'transform') return;
+}
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
