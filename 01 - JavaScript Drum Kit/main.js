019009cdfafb553684050a15e094bf02b4a0d51b
Pete Eichman <peichman14@gmail.com>
2017-03-06 15:48:05 -0500

    Added target to remove playing class so that the keys will flash and not stay on

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -9,6 +9,7 @@ window.addEventListener('keydown', function(e) {
 
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll('.key');
